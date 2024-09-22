f2731bbe712f870f74f214c40d0d15ccdf1c86f9
Sandeep Ghimire <deepsghimire1@gmail.com>
2024-09-22 22:00:48 +0545

    added antialiasing

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,11 @@ use std::rc::Rc;
 use tracer::Tracer;
 use tracing::{info, Level};
 use tracing_subscriber;
+use utils::random_vector;
 
 const ASPECT_RATIO: f32 = 16f32 / 9f32;
-const RECURSION_DEPTH: i32 = 6;
+const RECURSION_DEPTH: i32 = 3;
+const SAMPLING: i32 = 120;
 mod img;
 mod ray;
 mod scene;
@@ -115,7 +117,15 @@ fn main() -> Result<(), Box<dyn Error>> {
                 lerp_factor_width,
             );
             let pixel = Vec3A::lerp(t, b, lerp_factor_row);
-            let ray = ray::Ray::new(pixel, (pixel - camera_pos).normalize());
+            let ray_origin = pixel;
+            let ray_direction = (pixel - camera_pos).normalize();
+            let mut ray = ray::Ray::new(ray_origin, ray_direction);
+            let mut result_color = Vec3A::ZERO;
+            for _ in 0..SAMPLING {
+                ray = ray::Ray::new(ray_origin + random_vector(0.0, 0.02), ray_direction);
+                result_color += Tracer::trace(&ray, camera_pos, &scene, RECURSION_DEPTH)
+            }
+            result_color /= SAMPLING as f32;
             img_encoder.put_pixel_color(as_color(&Tracer::trace(
                 &ray,
                 camera_pos,
--- src/utils.rs
+++ src/utils.rs
@@ -7,3 +7,7 @@ pub fn as_color(v: &Vec3A) -> (u8, u8, u8) {
     let b = (b * 255.0) as u8;
     (r, g, b)
 }
+
+pub fn random_vector(min: f32, max: f32) -> Vec3A {
+    rand::random::<Vec3A>() * max + min
+}
